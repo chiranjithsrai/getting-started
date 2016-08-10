4539f4b7c5de1e6ad2b989efc831eaed113abe24
chiranjithsrai <chiranjithsrai@gmail.com>
2016-08-10 18:51:54 +0530

    created new maven junit project

---
+++ junit-maven/src/main/java/plavaga/junit/maven/App.java
@@ -0,0 +1,14 @@
+package plavaga.junit.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String printHello( )
+    {
+        System.out.println( "Hello World!" );
+        return "Hello World!";
+    }
+}
