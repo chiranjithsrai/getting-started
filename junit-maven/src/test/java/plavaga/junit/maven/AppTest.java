5727e6f309f876a8572caaa66ec2e733279f1384
chiranjithsrai <chiranjithsrai@gmail.com>
2016-08-11 16:14:37 +0530

    App test updated

--- junit-maven/src/test/java/plavaga/junit/maven/AppTest.java
+++ junit-maven/src/test/java/plavaga/junit/maven/AppTest.java
@@ -11,17 +11,34 @@ import org.junit.Test;
 public class AppTest 
 {
 	@Test
-    public void testSum()
+    public void test1Sum()
     {
 		App app = new App();
 		int result = app.getSum(5,10);
 		//Assert.assertEquals(result, 16);
 		if(result!=15)
 		{
+			//fail
 			Assert.fail("failed!"); 
 		}
 		else
+			//pass
 			System.out.println("Pass : Result -> " + result);
     }
-
+	
+	@Test
+    public void test2Sum()
+    {
+		App app = new App();
+		int result = app.getSum(6,10);
+		//Assert.assertEquals(result, 16);
+		if(result!=15)
+		{
+			//fail
+			Assert.fail("failed!"); 
+		}
+		else
+			//pass
+			System.out.println("Pass : Result -> " + result);
+    }
 }
