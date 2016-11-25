3e532c11233427403988d0ceea809e6d375c2700
chiranjithsrai <chiranjithsrai@gmail.com>
2016-11-25 13:26:55 +0530

    new MainApp class added.

---
+++ junit-maven/src/main/java/plavaga/junit/maven/MainApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package plavaga.junit.maven;
+
+/**
+ * @author chiranjithsrai
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Operations op = new Operations();
+		int num1 = 10;
+		int num2 = 5;
+		int sum = op.add(num1, num2);
+		System.out.println("Sum :: "+sum);
+	}
+
+}
