4434b6c687cdb247312be606211901a241abfd75
chiranjithsrai <chiranjithsrai@gmail.com>
2016-11-25 13:23:44 +0530

    new sample file added.

---
+++ junit-maven/src/main/java/plavaga/junit/maven/Operations.java
@@ -0,0 +1,12 @@
+package plavaga.junit.maven;
+
+/**
+ * @author chiranjithsrai
+ *
+ */
+public class Operations {
+	public int add(int num1, int num2) {
+		// addition
+		return num1 + num2;
+	}
+}
