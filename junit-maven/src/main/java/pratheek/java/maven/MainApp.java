9dc42822bc6ffbf1ee28f13899ddc96c2853370d
chiranjith <chiranjithsrai@gmail.com>
2016-12-21 01:27:44 +0530

    updated push

--- junit-maven/src/main/java/pratheek/java/maven/MainApp.java
+++ junit-maven/src/main/java/pratheek/java/maven/MainApp.java
@@ -3,6 +3,14 @@
  */
 package pratheek.java.maven;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * @author chiranjithsrai
  *
@@ -11,9 +19,64 @@ public class MainApp {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
-		System.out.println("hello");
+	public static void main(String[] args) throws IOException {
+		String emptyJson = "{}";
+		String newFilePath = "src/java/resources/newEntry.json";
+		String resultFilePath = "src/java/resources/record.json";
+
+		
+		File newEntry = readFile(newFilePath);
+		byte[] newData = getBytes(newEntry);
+		JSONObject newDataJsonObject = new JSONObject(new String(newData));
+		if (newDataJsonObject.length() == 0) {
+			System.out.println("no data");
+			System.exit(0);
+		}
+		writeToFile(newFilePath, emptyJson);
+
+		System.out.println("new entry :: " + newDataJsonObject);
+
+		File existingRecord = readFile(resultFilePath);
+		byte[] existing = getBytes(existingRecord);
+		JSONArray existingRecordJsonObject = new JSONArray(new String(existing));
+		existingRecordJsonObject.put(newDataJsonObject);
+		writeToFile(resultFilePath, existingRecordJsonObject.toString());
+		System.out.println("recorded");
+	}
+
+	/**
+	 * @param emptyJson
+	 * @param newFilePath
+	 * @throws IOException
+	 */
+	private static void writeToFile(String newFilePath, String emptyJson) throws IOException {
+		FileWriter fw = new FileWriter(newFilePath);
+		fw.write(emptyJson);
+		fw.close();
+	}
+
+	/**
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	private static byte[] getBytes(File file) throws IOException {
+		byte[] data = Files.readAllBytes(file.toPath());
+		return data;
+	}
+
+	/**
+	 * @return
+	 */
+	private static File readFile(String filename) {
+		File file = new File(filename);
+		if (!file.exists()) {
+			System.out.println("file not found");
+			System.exit(0);
+		}
+		return file;
 	}
 
 }
